882fe315dcfa40df6fff96f44c0b228121766489
Gonzalo Peñaherrera E. <gonzalop67@gmail.com>
2025-10-18 06:16:20 -0500

    17 - Asignar Etiquetas a Posts | Curso Blog desde Cero con Laravel 12

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
@@ -68,7 +69,9 @@ class PostController extends Controller
     public function edit(Post $post)
     {
         $categories = Category::all();
-        return view('admin.posts.edit', compact('post', 'categories'));
+        $tags = Tag::all();
+
+        return view('admin.posts.edit', compact('post', 'categories', 'tags'));
     }
 
     /**
@@ -83,6 +86,8 @@ class PostController extends Controller
             'excerpt' => 'nullable',
             'content' => 'nullable',
             'image' => 'nullable|image',
+            'tags' => 'nullable|array',
+            'tags.*' => 'exists:tags,id',
             'is_published' => 'required|boolean',
         ]);
 
@@ -95,6 +100,8 @@ class PostController extends Controller
         }
 
         $post->update($data);
+
+        $post->tags()->sync($data['tags'] ?? []);
 
         session()->flash('swal', [
             'icon' => 'success',
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -9,30 +9,33 @@
     <form action="{{ route('admin.posts.update', $post) }}" method="post" enctype="multipart/form-data">
         @csrf
         @method('PUT')
-        
+
         <div class="relative mb-2">
             @php
                 $ruta = dirname(dirname(__FILE__));
             @endphp
 
-            <img id="imgPreview" class="w-full aspect-video object-cover" src="{{ $post->image_path ? Storage::url($post->image_path) : "https://placehold.co/90x160?text=Imagen+Post" }}" alt="">
+            <img id="imgPreview" class="w-full aspect-video object-cover"
+                src="{{ $post->image_path ? Storage::url($post->image_path) : 'https://placehold.co/90x160?text=Imagen+Post' }}"
+                alt="">
 
             <div class="absolute top-8 right-8">
                 <label class="bg-white px-4 py-2 rounded-lg cursor-pointer">
                     Cambiar imagen
-                    <input class="hidden" type="file" name="image" accept="image/*" onchange="previewImage(event, '#imgPreview')">
+                    <input class="hidden" type="file" name="image" accept="image/*"
+                        onchange="previewImage(event, '#imgPreview')">
                 </label>
             </div>
         </div>
-        
+
         <div class="card" class="space-y-4">
-            <flux:input label="Título" name="title" value="{{ old('title', $post->title) }}"
+            <flux:input class="mb-4" label="Título" name="title" value="{{ old('title', $post->title) }}"
                 placeholder="Escriba el título del post" />
 
-            <flux:input label="Slug" id="slug" name="slug" value="{{ old('slug', $post->slug) }}"
-                placeholder="Escriba el slug del post" />
+            <flux:input class="mb-4" label="Slug" id="slug" name="slug"
+                value="{{ old('slug', $post->slug) }}" placeholder="Escriba el slug del post" />
 
-            <flux:select label="Categoría" name="category_id">
+            <flux:select class="mb-4" label="Categoría" name="category_id">
                 @foreach ($categories as $category)
                     <flux:select.option value="{{ $category->id }}"
                         :selected="$category->id == old('category_id', $post->category_id)">
@@ -41,25 +44,48 @@
                 @endforeach
             </flux:select>
 
-            <flux:textarea label="Resumen" name="excerpt">{{ old('excerpt', $post->excerpt) }}</flux:textarea>
-
-            <flux:textarea label="Contenido" name="content" rows="16">{{ old('content', $post->content) }}
+            <flux:textarea class="mb-4" label="Resumen" name="excerpt">{{ old('excerpt', $post->excerpt) }}
             </flux:textarea>
 
-            <div class="space-x-3">
-                <label>
-                    <input type="radio" name="is_published" value="0" @checked(old('is_published', $post->is_published) == 0)>
-                    <span class="ml-1">
-                        No pubicado
-                    </span>
-                </label>
+            <flux:textarea class="mb-4" label="Contenido" name="content" rows="16">
+                {{ old('content', $post->content) }}
+            </flux:textarea>
 
-                <label>
-                    <input type="radio" name="is_published" value="1" @checked(old('is_published', $post->is_published) == 1)>
-                    <span class="ml-1">
-                        Pubicado
-                    </span>
-                </label>
+            <div class="mb-4">
+                <p class="text-sm font-medium mb-1">Etiquetas</p>
+
+                <ul>
+                    @foreach ($tags as $tag)
+                        <li>
+                            <label class="flex items-center space-x-2">
+                                <input type="checkbox" name="tags[]" 
+                                value="{{ $tag->id }}"
+                                @checked(in_array($tag->id, old('tags', $post->tags->pluck('id')->toArray())))
+                                >
+                                <span>{{ $tag->name }}</span>
+                            </label>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+
+            <div class="mb-4">
+                <p class="text-sm font-medium mb-1">Estado</p>
+                <div class="space-x-3">
+                    <label>
+                        <input type="radio" name="is_published" value="0" @checked(old('is_published', $post->is_published) == 0)>
+                        <span class="ml-1">
+                            No pubicado
+                        </span>
+                    </label>
+
+                    <label>
+                        <input type="radio" name="is_published" value="1" @checked(old('is_published', $post->is_published) == 1)>
+                        <span class="ml-1">
+                            Pubicado
+                        </span>
+                    </label>
+                </div>
             </div>
 
             <div class="flex justify-end">
